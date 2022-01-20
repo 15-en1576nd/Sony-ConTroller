ae2c770c036aed41a61254200d29ee4b291120d1
marnixah <marnix@hagehoeve.com>
2022-01-20 15:18:40 +0100

    feat: post /tv/:ip

--- Routes/tv.js
+++ Routes/tv.js
@@ -33,10 +33,32 @@ router.get("/tv", async (req, res) => {
  *        in: path
  *        description: TV ip
  *        required: true
+ *        type: string
+ *  post:
+ *    description: Send an IRCC command to a TV
+ *    tags: [TV]
+ *    parameters:
+ *      - name: ip
+ *        in: path
+ *        description: TV ip
+ *        required: true
+ *        type: string
+ *      - name: command
+ *        in: body
+ *        description: IRCC command
+ *        required: true
+ *        type: string
  */
 router.get("/tv/:ip", async (req, res) => {
   const ip = req.params.ip;
   const tv = new Bravia(ip, "80", "0000");
   return res.json(await tv.getIRCCCodes());
 });
+router.post("/tv/:ip", async (req, res) => {
+  const ip = req.params.ip;
+  const tv = new Bravia(ip, "80", "0000");
+  const command = req.body.command;
+  return res.json(await tv.sendIRCC(command));
+});
+
 module.exports = router;
